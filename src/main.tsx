7f8317e28a506f796f51885e2a28036297ab0eec
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-23 15:38:33 +0200

    feat(static-pages): first react code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+// import ReactDOM from "react-dom/client"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// ReactDOM.createRoot(document.querySelector("#root")).render(<p>Hello from the world of React!</p>)
+// createRoot(document.querySelector("#root")).render(<p>Hello from the world of React!</p>)
+const root = createRoot(document.querySelector("#root"));
+root.render(<p>Hello from the world of React!</p>);
