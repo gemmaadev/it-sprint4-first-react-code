c39d7fb225c57519fcc535f5fd71992dfc478332
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-23 16:06:03 +0200

    feat(static-pages): first react challenge

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,11 @@ import { createRoot } from "react-dom/client";
 
 // ReactDOM.createRoot(document.querySelector("#root")).render(<p>Hello from the world of React!</p>)
 // createRoot(document.querySelector("#root")).render(<p>Hello from the world of React!</p>)
-const root = createRoot(document.querySelector("#root"));
-root.render(<p>Hello from the world of React!</p>);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <ul>
+    <li>Super popular JS library</li>
+    <li>Will help me be even more employable</li>
+    <li>How widely it’s used in real-world products</li>
+  </ul>,
+);
