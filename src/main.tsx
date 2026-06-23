02f31d68449d7706846fc29701c128ec88762b82
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-23 16:34:26 +0200

    feat(static-pages): React.createElement()

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,23 @@ root.render(
     <li>How widely it’s used in real-world products</li>
   </ul>,
 );
+////Components and JSX are ultimately transformed into JavaScript objects that React interprets to build the user interface.
+
+
+
+// This is how React elements were created in the early days, before JSX became the standard way of writing components:
+
+// import { createElement } from "react"
+// import { createRoot } from "react-dom/client"
+
+// const root = createRoot(document.getElementById("root"))
+// const reactElement = createElement("h1", null, "Hello from createElement!") //Null is the prop
+
+// console.log(reactElement)
+//the console shows this: {type: 'h1', key: null, props: {children: 'Hello from createElement!'}, _owner: null, _store: {}}
+
+// root.render(
+//     reactElement
+// )
+
+
