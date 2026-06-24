899309ebf0ef554956ed6fcdf003d3fd66482b46
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-24 13:53:55 +0200

    feat(static-pages): React declarative

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { MyAwesomeNavbar } from "./components/navbar";
+
 const root = createRoot(document.getElementById("root"));
 
 export function MainContent() {
@@ -20,3 +21,28 @@ root.render(
     <MainContent />
   </div>,
 );
+
+ 
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element (createElement)
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child (using `appendChild`) of the div#root
+
+Don't use innerHTML to accomplish any of this.
+    
+// */
+// root.render(
+//     <h1 className="header">Hello, React!</h1>
+// )
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is imperative coding"
+// h1.className = "header"
+// document.getElementById("root").appendChild(h1) 
+
+//THIS IS IMPERATIVE "How should it be done, step by step"
