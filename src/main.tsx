5c9b80b4ac209d88856bac67dc55f9b5d21b0479
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-24 21:05:07 +0200

    feat(static-pages): fragments

--- src/main.tsx
+++ src/main.tsx
@@ -1,77 +1,84 @@
+// import { createRoot } from "react-dom/client";
+// import { MyAwesomeNavbar } from "./components/navbar";
+
+// const root = createRoot(document.getElementById("root"));
+
+// export function MainContent() {
+//   return (
+//     <main>
+//       <h1>Fun facts about React!</h1>
+//       <ul>
+//         <li>Was first release in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 200K stars on GitHub</li>
+//         <li>Is maintained by Meta</li>
+//         <li>Powers thousands of enterprise apps, including mobile apps</li>
+//       </ul>
+//     </main>
+//   );
+// }
+
+// export function SecondaryContent() {
+//   return (
+//     <section>
+//       <h2>Why I am excited to be learning React?</h2>
+//       <ol>
+//         <li>Super popular JS library</li>
+//         <li>Will help me be even more employable</li>
+//         <li>How widely it’s used in real-world products</li>
+//       </ol>
+//     </section>
+//   );
+// }
+
+// export function Footer() {
+//   return (
+//     <footer>
+//       <small>© 2026 Maeso development. All rights reserved.</small>
+//     </footer>
+//   );
+// }
+
+// root.render(
+//   <div>
+//     <MyAwesomeNavbar />
+//     <MainContent />
+//     <SecondaryContent />
+//     <Footer />
+//   </div>,
+// );
+
 import { createRoot } from "react-dom/client";
-import { MyAwesomeNavbar } from "./components/navbar";
+// import { Fragment } from "react";
 
 const root = createRoot(document.getElementById("root"));
 
-export function MainContent() {
+export function Page() {
   return (
-    <main>
-      <h1>Fun facts about React!</h1>
-      <ul>
-        <li>Was first release in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
+    // <Fragment>
+    <>
+      <header>
+        <img src="react-logo.png" width="40px" alt="React logo" />
+      </header>
+      <main>
+        <h1>Reason I am excited to learn React</h1>
+        <ol>
+          <li>
+            React is a popular library, so I will be able to fit in with all the
+            coolest devs out there! 😎
+          </li>
+          <li>
+            I am more likely to get a job as a front end developer if I know
+            React
+          </li>
+        </ol>
+      </main>
+      <footer>
+        <small>© 2024 Ziroll development. All rights reserved.</small>
+      </footer>
+      {/* </Fragment>  */}
+    </>
   );
 }
 
-export function SecondaryContent() {
-  return (
-    <section>
-      <h2>Why I am excited to be learning React?</h2>
-      <ol>
-        <li>Super popular JS library</li>
-        <li>Will help me be even more employable</li>
-        <li>How widely it’s used in real-world products</li>
-      </ol>
-    </section>
-  );
-}
-
-export function Footer() {
-  return (
-    <footer>
-      <small>© 2026 Maeso development. All rights reserved.</small>
-    </footer>
-  );
-}
-
-root.render(
-  <div>
-    <MyAwesomeNavbar />
-    <MainContent />
-    <SecondaryContent />
-    <Footer />
-  </div>,
-);
-
- 
-//QUIZ
-
-// 1. What is a React component?
-// A function that returns React elements. (UI)
-
-// 2. What's wrong with this code?
-// ```
-// function myComponent() {  >>>>>  MyComponent()
-//     return (
-//         <small>I'm tiny text!</small>
-//     )
-// }
-// ```
-
-// 3. What's wrong with this code?
-// ```
-// function Header() {
-//     return (
-//         <header>
-//             <img src="./react-logo.png" width="40px" alt="React logo" />
-//         </header>
-//     )
-// }
-
-// root.render(Header())  >>>>>  <Header />
-// ```
+root.render(<Page />);
